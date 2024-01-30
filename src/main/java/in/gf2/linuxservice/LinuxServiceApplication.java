a226ec7b4ace46a71fd24cf43988681e2694f1f7
Gabriel F Guimaraes de Freitas <ggfto@outlook.com>
2024-01-29 21:09:23 -0300

    auto release test

--- src/main/java/in/gf2/linuxservice/LinuxServiceApplication.java
+++ src/main/java/in/gf2/linuxservice/LinuxServiceApplication.java
@@ -3,11 +3,15 @@ package in.gf2.linuxservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class LinuxServiceApplication {
-	
+
 
 public static void main(String[] args) {
 		SpringApplication.run(LinuxServiceApplication.class, args);
+        log.info("It works!");
 	}
 }
